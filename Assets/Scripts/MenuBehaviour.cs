2574a489e739fb13d5ced91462baa8bd2fb8e9fa
KarmicSine68 <117841894+KarmicSine68@users.noreply.github.com>
2026-05-22 17:01:57 -0500

    Input Functions

---
+++ Assets/Scripts/MenuBehaviour.cs
@@ -0,0 +1,35 @@
+/******************************************************************************
+ * Author: Brad Dixon
+ * Creation Date: 5/22/2026
+ * Last Modified: 5/22/2026
+ * Brief: Handles the button functions for menus.
+ * External Resources: N/A
+ * ***************************************************************************/
+using UnityEngine;
+
+public class MenuBehaviour : MonoBehaviour
+{
+    /// <summary>
+    /// Test function that loads the movement test.
+    /// </summary>
+    public void TestMovement()
+    {
+
+    }
+
+    /// <summary>
+    /// Test function that loads the dialogue test.
+    /// </summary>
+    public void TestDialogue()
+    {
+
+    }
+
+    /// <summary>
+    /// Test function that loads the rhythm test.
+    /// </summary>
+    public void TestRhythm()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,133 @@
+/******************************************************************************
+ * Author: Brad Dixon
+ * Creation Date: 5/22/2026
+ * Last Modified: 5/22/2026
+ * Brief: Handles the player's controls.
+ * External Resources: N/A
+ * ***************************************************************************/
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public enum GameplayStates
+    {
+        World,
+        Dialogue,
+        Rhythm
+    }
+
+    [SerializeField] private GameplayStates playerState;
+
+    PlayerInput pInput;
+    InputAction pMove, pInteract, pClick;
+    InputAction note1, note2, note3, note4;
+
+    [Tooltip("How fast the player moves.")]
+    [SerializeField] private float playerSpeed;
+
+    Rigidbody rb;
+
+    /// <summary>
+    /// Assignes components and values
+    /// </summary>
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        pInput = GetComponent<PlayerInput>();
+
+        pInput.currentActionMap.Enable();
+        pMove = pInput.currentActionMap.FindAction("Move");
+        pInteract = pInput.currentActionMap.FindAction("Interact");
+        pClick = pInput.currentActionMap.FindAction("Click");
+        note1 = pInput.currentActionMap.FindAction("Note1");
+        note2 = pInput.currentActionMap.FindAction("Note2");
+        note3 = pInput.currentActionMap.FindAction("Note3");
+        note4 = pInput.currentActionMap.FindAction("Note4");
+    }
+
+    /// <summary>
+    /// Enables input actions
+    /// </summary>
+    private void OnEnable()
+    {
+        pMove.performed += PMove_performed;
+        pInteract.started += PInteract_started;
+        pClick.started += PClick_started;
+        note1.started += Note1_started; note1.canceled += Note1_canceled;
+        note2.started += Note2_started; note2.canceled += Note2_canceled;
+        note3.started += Note3_started; note3.canceled += Note3_canceled;
+        note4.started += Note4_started; note4.canceled += Note4_canceled;
+    }
+
+    /// <summary>
+    /// Disables the input system
+    /// </summary>
+    private void OnDisable()
+    {
+        pMove.performed -= PMove_performed;
+        pInteract.started -= PInteract_started;
+        pClick.started -= PClick_started;
+        note1.started -= Note1_started; note1.canceled -= Note1_canceled;
+        note2.started -= Note2_started; note2.canceled -= Note2_canceled;
+        note3.started -= Note3_started; note3.canceled -= Note3_canceled;
+        note4.started -= Note4_started; note4.canceled -= Note4_canceled;
+    }
+
+    #region InputFunctions
+    private void PMove_performed(InputAction.CallbackContext obj)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void PInteract_started(InputAction.CallbackContext obj)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void PClick_started(InputAction.CallbackContext obj)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void Note1_started(InputAction.CallbackContext obj)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void Note1_canceled(InputAction.CallbackContext obj)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void Note2_started(InputAction.CallbackContext obj)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void Note2_canceled(InputAction.CallbackContext obj)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void Note3_started(InputAction.CallbackContext obj)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void Note3_canceled(InputAction.CallbackContext obj)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void Note4_started(InputAction.CallbackContext obj)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void Note4_canceled(InputAction.CallbackContext obj)
+    {
+        throw new System.NotImplementedException();
+    }
+    #endregion
+}
